c3c96d49ff3edd89c4df31df87165a7a5325feef
Philip T Dodd <phil.dodd@cinnamonapps.co.uk>
2023-02-16 07:08:04 +0000

    Refactored the peekMove method and moved to Heading

--- src/main/java/mars/rover/Heading.java
+++ src/main/java/mars/rover/Heading.java
@@ -38,5 +38,38 @@ public class Heading extends GridReference {
 
     public String getHeadingLetter() {
         return heading.letter;
+
+    }
+        
+    protected Heading peekMove(Integer xMax, Integer yMax) {
+        Integer newX = getX();
+        Integer newY = getY();
+        Compass newHeading = heading;
+
+        if (heading == Compass.NORTH)
+            newY++;
+        else if (heading == Compass.EAST)
+            newX++;
+        else if (heading == Compass.SOUTH)
+            newY--;
+        else if (heading == Compass.WEST)
+            newX--;
+        else
+            return (new Heading(getX(), getY(), heading));
+
+        if (newX == -1)
+            newX = xMax;
+        else if (newX == xMax)
+            newX = 0;
+
+        if (newY == -1) {
+            newY = yMax;
+            newHeading = Compass.NORTH;
+        } else if (newY == yMax) {
+            newY = 0;
+            newHeading = Compass.SOUTH;
+        }
+
+        return (new Heading(newX, newY, newHeading));
     }
 }
